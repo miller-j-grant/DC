c003170b7f7d96bfedb6718aef5113e9cbd8b974
Patrick Howard Dishaw <paratuelvenatir@gmail.com>
2016-02-09 05:05:10 -0500

    Constructors and basic structure idea for them!

---
+++ Dungeon Crawler +/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler__
+{
+    class Character
+    {
+        /* Every character has the following:
+            Name
+            Race
+                Speed, Languages
+            Class
+                Proficiencies, Spells
+            Ability scores (str, dex, con, intel, wis, cha)
+                Hit points, hit dice, proficiency bonus, initiative, passive wisdom
+            Background
+            Equipment
+                AC, attack bonus, packs and packs and packs and packs, $$ALL THAT SICK GP YO$$
+        */
+    }
+}
---
+++ Dungeon Crawler +/Race.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+namespace Dungeon_Crawler__
+{
+
+
+    class Race
+    {
+
+        // racial ability score bonuses
+        private int str;
+        private int dex;
+        private int con;
+        private int intel;
+        private int wis;
+        private int cha;
+
+        // size could either be a String that we read or a number that we correspond with a size
+        // might be easier to do number, but we'll see
+        private String size;
+
+        private int speed;
+
+        private String traits;
+
+        private String languages;
+
+        // this might be dumb, but it's like 3:45am so whatever
+        private string subrace;
+
+        public Race()
+        {
+            str = 0;
+            dex = 0;
+            con = 0;
+            intel = 0;
+            wis = 0;
+            cha = 0;
+
+            size = "Medium";
+
+            speed = 30;
+
+            languages = "Common";
+        }
+
+        public Race(string race)
+        {
+            if (race.Equals("Elf")){
+                dex = 2;
+                size = "Medium";
+                speed = 30;
+                traits = "Darkvision, Keen Senses, Fey Ancestry, Trance";
+                languages = "Common, Elvish";
+                subrace = "High Elf, Wood Elves, Drow";
+            }
+
+            if (race.Equals("Dwarf"))
+            {
+
+            }
+
+        }
+    }
+}
